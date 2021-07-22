a2052c167f6aa4b79afc2b8b6c69b81b698907db
Byeongju-Kong <bjuuuu98@gmail.com>
2021-07-22 16:32:44 +0900

    refactor : Main 객체의 main 함수에서 Calculation 객체가 아닌 Operation 객체를 생성하는 것으로 수정

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,11 +1,12 @@
 package controller;
 
 import model.Calculator;
+import model.Operation;
 import view.Result;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator calculator = new Calculation();
+        Calculator calculator = new Operation();
         calculator.input();
         calculator.calculate();
         Result.showResult(calculator);
