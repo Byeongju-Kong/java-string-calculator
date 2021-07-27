a2dc2e8d89bd06e5250ae3daeaadbdc6047aa046
Byeongju-Kong <bjuuuu98@gmail.com>
2021-07-27 15:45:30 +0900

    feat : Operands, Operand, Operator의 테스트 코드 수정

---
+++ src/test/java/model/OperandTest.java
@@ -0,0 +1,34 @@
+package model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class OperandTest {
+    private Operand operand;
+
+    @Test
+    @DisplayName("Operand 객체가 잘 생성되는지 Test")
+    public void create() {
+        operand = new Operand("1");
+        assertThat(operand.getOperand()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("실-수로 Operand 객체를 생성했을 시에 예외 발생 Test")
+    public void create_ExceptionByActualNumber() {
+        assertThatThrownBy(() -> new Operand("2.5"))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("정수 자리에 잘못된 입력이 있습니다");
+    }
+
+    @Test
+    @DisplayName("기호로 Operand 객체를 생성했을 시에 예외 발생 Test")
+    public void create_ExceptionBySymbol() {
+        assertThatThrownBy(() -> new Operand("+"))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("정수 자리에 잘못된 입력이 있습니다");
+    }
+}
---
+++ src/test/java/model/OperatorTest.java
@@ -0,0 +1,33 @@
+package model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class OperatorTest {
+    private Operator operator;
+    @Test
+    @DisplayName("Operator 생성이 잘되는지 Test")
+    public void create() {
+        operator = new Operator("+");
+        assertThat(operator.getOperator()).isEqualTo('+');
+    }
+
+    @Test
+    @DisplayName("Operator 생성시 숫자가 들어가면 예외 발생 Test")
+    public void create_ExceptionByNumber() {
+        assertThatThrownBy(()-> new Operator("5"))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("연산기호에 잘못된 입력이 있습니다");
+    }
+
+    @Test
+    @DisplayName("Operator 생성시 숫자가 들어가면 예외 발생 Test")
+    public void create_ExceptionInvalidSymbol() {
+        assertThatThrownBy(()-> new Operator("["))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("연산기호에 잘못된 입력이 있습니다");
+    }
+}
