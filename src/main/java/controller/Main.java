ea4b627904275532259ff405b459863a55cb579e
Byeongju-Kong <bjuuuu98@gmail.com>
2021-07-29 16:11:45 +0900

    refactor : Operation 클래스 이름 변경
    Calculator 인퍼페이스 삭제 후
    Operation.java -> Calculator.java

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,12 +1,11 @@
 package controller;
 
 import model.Calculator;
-import model.Operation;
 import view.Result;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator calculator = new Operation();
+        Calculator calculator = new Calculator();
         calculator.input();
         calculator.calculate();
         Result.showResult(calculator);
