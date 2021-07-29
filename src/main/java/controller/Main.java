f32c1368b31f5d03cbf953c4e97996b266d649ad
Byeongju-Kong <bjuuuu98@gmail.com>
2021-07-29 17:30:55 +0900

    refactor : 입력 안내문 출력하는 책임을 가지는 객체 변경,
    Input -> Result

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -6,6 +6,7 @@ import view.Result;
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
+        Result.alertInput();
         calculator.input();
         calculator.calculate();
         Result.showResult(calculator);
--- src/main/java/view/Result.java
+++ src/main/java/view/Result.java
@@ -6,4 +6,7 @@ public class Result {
     public static void showResult(Calculator calculator) {
         System.out.printf("계산결과는 %.1f입니다.", calculator.getResult());
     }
+    public static void alertInput() {
+        System.out.print("계산하려는 식을 입력하세요 : ");
+    }
 }
