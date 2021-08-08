ee81d2ce76f1a051b348e3cc15f69aec8c0f1d7e
Byeongju-Kong <bjuuuu98@gmail.com>
2021-08-08 21:48:40 +0900

    refactor : Operator Enum으로 수정

--- src/test/java/model/OperatorTest.java
+++ src/test/java/model/OperatorTest.java
@@ -1,6 +1,6 @@
 package model;
 
-import model.Operator.Operator;
+import model.operator.Operator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,24 +11,24 @@ class OperatorTest {
     private Operator operator;
     @Test
     @DisplayName("Operator 생성이 잘되는지 Test")
-    public void create() {
-        operator = new Operator("+");
+    void create() {
+        operator = Operator.of('+');
         assertThat(operator.getOperator()).isEqualTo('+');
     }
 
     @Test
     @DisplayName("Operator 생성시 숫자가 들어가면 예외 발생 Test")
-    public void create_ExceptionByNumber() {
-        assertThatThrownBy(()-> new Operator("5"))
-                .isInstanceOf(IllegalStateException.class)
+    void create_ExceptionByNumber() {
+        assertThatThrownBy(()-> Operator.of('5'))
+                .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("연산기호에 잘못된 입력이 있습니다");
     }
 
     @Test
     @DisplayName("Operator 생성시 숫자가 들어가면 예외 발생 Test")
-    public void create_ExceptionInvalidSymbol() {
-        assertThatThrownBy(()-> new Operator("["))
-                .isInstanceOf(IllegalStateException.class)
+    void create_ExceptionInvalidSymbol() {
+        assertThatThrownBy(()-> Operator.of('['))
+                .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("연산기호에 잘못된 입력이 있습니다");
     }
 }
