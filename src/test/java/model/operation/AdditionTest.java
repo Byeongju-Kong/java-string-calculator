45918a89d5e0672bf95377dd418aad085e2f21a0
Byeongju-Kong <bjuuuu98@gmail.com>
2021-08-08 22:05:03 +0900

    feat : Addition 테스트 및 구현

---
+++ src/test/java/model/operation/AdditionTest.java
@@ -0,0 +1,29 @@
+package model.operation;
+
+import model.operator.Operator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AdditionTest {
+    private Operation addition;
+
+    @Test
+    @DisplayName("+를 가지는 Operator 객체를 받아 true를 반환한다.")
+    void hasOperation_true() {
+        addition = new Addition();
+        assertThat(addition.hasOperation(Operator.of('+'))).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("+이외의 Operator 객체를 받아 false를 반환한다.")
+    @ValueSource(chars = {'-', '*', '/'})
+    void hasOperation_false(char invalidSource) {
+        addition = new Addition();
+        assertThat(addition.hasOperation(Operator.of(invalidSource))).isFalse();
+    }
+
+}
