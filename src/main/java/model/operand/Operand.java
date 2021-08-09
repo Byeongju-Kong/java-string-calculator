544ecfe1e28316cfb94bff3c3801e97991c28ebf
Byeongju-Kong <bjuuuu98@gmail.com>
2021-08-09 16:37:52 +0900

    refactor : 정규식에 대한 지역변수 private static final 필드로 수정

--- src/main/java/model/operand/Operand.java
+++ src/main/java/model/operand/Operand.java
@@ -2,9 +2,9 @@ package model.operand;
 
 public class Operand {
     private final int operand;
+    private static final String OPERAND_REGEX = "^[0-9]*$";
 
     public Operand(String operand) {
-        String OPERAND_REGEX = "^[0-9]*$";
         if (!operand.matches(OPERAND_REGEX)) {
             throw new IllegalStateException("정수 자리에 잘못된 입력이 있습니다");
         }
