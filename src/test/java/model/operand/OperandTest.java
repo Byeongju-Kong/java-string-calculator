a4386bb64e5ee9407c6b0670210c0dbf7775a3af
Byeongju-Kong <bjuuuu98@gmail.com>
2021-08-09 16:27:55 +0900

    refactor : ParameterizedTest와 ValueSource로 두 개의 테스트 케이스 묶음

--- src/test/java/model/operand/OperandTest.java
+++ src/test/java/model/operand/OperandTest.java
@@ -3,6 +3,8 @@ package model.operand;
 import model.Operand.Operand;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -17,18 +19,11 @@ class OperandTest {
         assertThat(operand.getOperand()).isEqualTo(1);
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("실-수로 Operand 객체를 생성했을 시에 예외 발생 Test")
-    void create_ExceptionByActualNumber() {
-        assertThatThrownBy(() -> new Operand("2.5"))
-                .isInstanceOf(IllegalStateException.class)
-                .hasMessage("정수 자리에 잘못된 입력이 있습니다");
-    }
-
-    @Test
-    @DisplayName("기호로 Operand 객체를 생성했을 시에 예외 발생 Test")
-    void create_ExceptionBySymbol() {
-        assertThatThrownBy(() -> new Operand("+"))
+    @ValueSource(strings = {"2.5", "+"})
+    void create_ExceptionByActualNumber(String wrongOperand) {
+        assertThatThrownBy(() -> new Operand(wrongOperand))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessage("정수 자리에 잘못된 입력이 있습니다");
     }
