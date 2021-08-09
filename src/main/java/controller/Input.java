cef730230a5b2e3a22e210c1bb651833a0ac8894
Byeongju-Kong <bjuuuu98@gmail.com>
2021-08-09 15:57:37 +0900

    refactor : Input 클래스 생성자의 접근제어 private으로 지정

--- src/main/java/controller/Input.java
+++ src/main/java/controller/Input.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class Input {
     private static final Scanner SCANNER = new Scanner(System.in);
 
+    private Input() {
+    }
+
     public static String input() {
         String inputString = SCANNER.nextLine().trim();
         SCANNER.close();
