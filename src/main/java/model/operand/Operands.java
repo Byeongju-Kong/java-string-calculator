9b29b07070017d38ce3cf07f95d278ced49e8151
Byeongju-Kong <bjuuuu98@gmail.com>
2021-08-10 13:42:44 +0900

    refactor : 필드 이름 변경

--- src/main/java/model/operand/Operands.java
+++ src/main/java/model/operand/Operands.java
@@ -5,11 +5,11 @@ import java.util.Queue;
 import java.util.stream.IntStream;
 
 public class Operands {
-    private Queue<Operand> operands;
+    private Queue<Operand> numbers;
     private static final int START_ENTRY = 0;
 
     public Operands(String[] operationSource) {
-        operands = parseOperand(operationSource);
+        numbers = parseOperand(operationSource);
     }
 
     private Queue<Operand> parseOperand(String[] operationSource) {
@@ -25,9 +25,9 @@ public class Operands {
     }
 
     public int getSource() {
-        if (operands.size() == 0) {
+        if (numbers.size() == 0) {
             throw new IndexOutOfBoundsException("반환할 숫자가 없습니다.");
         }
-        return operands.remove().getOperand();
+        return numbers.remove().getOperand();
     }
 }
