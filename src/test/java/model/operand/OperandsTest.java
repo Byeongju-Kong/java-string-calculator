85bf3816ff9cfc7ce39b8c87ea78ec2557069cf3
Byeongju-Kong <bjuuuu98@gmail.com>
2021-08-10 11:22:08 +0900

    refactor : 테스트 케이스 추가
    IndexOutOfBounds에 해당하는 예외 추가

--- src/test/java/model/operand/OperandsTest.java
+++ src/test/java/model/operand/OperandsTest.java
@@ -3,6 +3,7 @@ package model.operand;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -18,5 +19,16 @@ class OperandsTest {
                 () -> assertEquals(operands.getSource(), 3),
                 () -> assertEquals(operands.getSource(), -5)
         );
+    }
+
+    @Test
+    @DisplayName("Queue의 size가 0인데, getSource를 호출하면 예외를 발생시킨다.")
+    void getSource_ExceptionByIndexOutOfBounds() {
+        operands = new Operands(new String[]{"1", "+", "3"});
+        operands.getSource();
+        operands.getSource();
+        assertThatThrownBy(() -> operands.getSource())
+                .isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessage("반환할 숫자가 없습니다.");
     }
 }
