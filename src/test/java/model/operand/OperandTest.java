9d001a31106e397909198d2b1fa610a58c0cdc40
Byeongju-Kong <bjuuuu98@gmail.com>
2021-08-10 12:38:03 +0900

    refactor : CsvSource로 테스트 수정

--- src/test/java/model/operand/OperandTest.java
+++ src/test/java/model/operand/OperandTest.java
@@ -2,15 +2,11 @@ package model.operand;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class OperandTest {
     @ParameterizedTest
@@ -19,14 +15,6 @@ class OperandTest {
     void create(String givenOperand, int returnedOperand) {
         Operand operand = new Operand(givenOperand);
         assertThat(operand.getOperand()).isEqualTo(returnedOperand);
-    }
-
-    static Stream<Arguments> provideOperand() {
-        return Stream.of(
-                arguments("1", 1),
-                arguments("-2", -2),
-                arguments("100", 100)
-        );
     }
 
     @ParameterizedTest
